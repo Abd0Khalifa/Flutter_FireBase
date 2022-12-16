7dd2425b353bd02f7467e44009e6543191b41cc9
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-17 01:31:53 +0200

    Abdulrahman Khalifa Creat CustomButton

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -14,14 +14,33 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25,horizontal:35 ),
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
             child: Column(
               children: [
                 Image.asset(
                   "assets/1.png",
                   height: 300,
                 ),
-                
+                const SizedBox(height: 20),
+                const Text(
+                  "Let's get Started",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Never a better time than now to start.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
               ],
             ),
           ),

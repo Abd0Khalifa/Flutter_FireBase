8eac441ac94e4937bb4ba774f58b2762d18187db
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-17 01:09:30 +0200

    Abdulrahman Khalifa Starting Create welcome Screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25,horizontal:35 ),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/1.png",
+                  height: 300,
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

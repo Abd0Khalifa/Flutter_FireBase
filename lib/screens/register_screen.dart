52ddcfa72b3b1a5fb3cc1562ca60521dde22aa4a
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-17 15:15:36 +0200

    Abdulrahman Khalifa Finish Register page Design

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/widgets/custem_button.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -139,6 +140,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
                 const SizedBox(
                   height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(text: "Login", onPressed: () {}),
                 ),
               ],
             ),

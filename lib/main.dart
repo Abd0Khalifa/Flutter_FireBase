b23d20c4c5be55b5bb5063490fc364b0e5de992c
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-19 00:00:25 +0200

    Abdulrahman Khalifa  Create the Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/provider/auth_provider.dart';
 import 'package:flutter_firebase/screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,10 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
-      title: "PhoneAuth",
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WelcomeScreen(),
+        title: "PhoneAuth",
+      ),
     );
   }
 }

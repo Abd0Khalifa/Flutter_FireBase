6402c22031cba0b4279adfc2d9737f2f85256547
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-19 18:51:19 +0200

    Abdulrahman Khalifa Start in UserInfoScreen Design

--- lib/screens/user_info_screen.dart
+++ lib/screens/user_info_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class UserInformationScreen extends StatefulWidget {
@@ -8,8 +10,72 @@ class UserInformationScreen extends StatefulWidget {
 }
 
 class _UserInformationScreenState extends State<UserInformationScreen> {
+  File? image;
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final bioController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    nameController.dispose();
+    emailController.dispose();
+    bioController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 5.0),
+          child: Center(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: image == null
+                      ? const CircleAvatar(
+                          backgroundColor: Colors.purple,
+                          radius: 50,
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        )
+                      : CircleAvatar(
+                          backgroundImage: FileImage(image!),
+                          radius: 50,
+                        ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                  child: Column(
+                    children: [],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textfeld({
+    required String hintText,
+    required IconData icon,
+    required TextInputType inputType,
+    required int maxLines,
+    required TextEditingController controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextFormField(),
+    );
   }
 }

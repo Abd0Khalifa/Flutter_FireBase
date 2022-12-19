d8fd038a3eb8c710fb6e5c132d25cca9492fbb49
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-19 22:54:30 +0200

    Abdulrahman Khalifa HomeScreen Design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,8 +12,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+
     return Scaffold(
-      body: Center(child: Text("HOMESCREEN")),
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("FlutterPhone Auth"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.purple,
+              backgroundImage: NetworkImage(ap.userModel.profilPic),
+              radius: 70,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(ap.userModel.name),
+            Text(ap.userModel.phoneNumber),
+            Text(ap.userModel.email),
+            Text(ap.userModel.bio),
+          ],
+        ),
+      ),
     );
   }
 }
